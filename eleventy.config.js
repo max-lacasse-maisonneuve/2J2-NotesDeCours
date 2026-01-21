61818297875a495effcfae75ebd4a5d52069314d
egl-edu <egl-edu@proton.me>
2026-01-21 13:25:06 -0500

    Correction de l'ordre des notes dans la liste initiale

--- eleventy.config.js
+++ eleventy.config.js
@@ -19,7 +19,17 @@ export default async function (eleventyConfig)
     // Filtrer les sources utilisées
     eleventyConfig.addCollection("notes", async (collectionsApi) => {
         // get unsorted items
-        let notes = collectionsApi.getFilteredByGlob(["[0-9]*_*.md"]);
+        let notes = collectionsApi.getFilteredByGlob(["[0-9]*_*.md"]).sort(function (a, b)
+        {
+            if (a.fileSlug < b.fileSlug)
+            {
+                return -1
+            }
+            else
+            {
+                return 1
+            }
+        });
         //console.log(notes);
 		return notes;
 	});
