340129a229a2b974ad2834d97450b6c4e801a7c2
Enric Granzotto Llagostera <egl-edu@proton.me>
2026-02-10 12:44:04 -0500

    Polices custom

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
 import LayoutNotes from './LayoutNotes.vue'
 
+import './custom-fonts.css';
+
 export default {
     extends: DefaultTheme,
     // override the Layout with a wrapper component that
